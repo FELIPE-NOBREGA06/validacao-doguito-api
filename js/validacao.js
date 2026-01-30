55b9fa6b067d4d8b3aa1364b52550d3202ea4d92
CLEVERSON  SANTOS <felipecleverson1@gmail.com>
2026-01-30 10:36:08 -0400

    feat: mensagem customizada

--- js/validacao.js
+++ js/validacao.js
@@ -1,62 +1,73 @@
-const validadores = {
-  dataNascimento: (input) => validadeDataNascimento(input),
-};
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
 
-function valida(input) {
-  const tipoInput = input.dataset.tipo;
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
 
-  if (validadores[tipoInput]) {
-    validadores[tipoInput](input);
-  }
-
-  if (input.validity.valid) {
-    input.parentElement.classList.remove("input-container--invalido");
-  } else {
-    input.parentElement.classList.add("input-container--invalido");
-  }
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
 }
 
-const mensagemDeErro = {
-  nome: {
-    valueMissing: "O campo não pode estar vazio  ",
-  },
-  email: {
-    valueMissing: "O campo de email não pode estar vazio ",
-    typeMismatch: "O e-mail digitado não e valido",
-  },
-  senha: {
-    valueMissing: "O campo de senha não pode estar vazio",
-    patternMismatch:
-      "A senha deve conter 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos ",
-  },
-  dataNascimento: {
-    valueMissing: "O campo de data de nascimento não pode estar vazio",
-    customError: "Você deve ser maior de 18 anos para se cadastrar",
-  },
-};
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
 
-dataNascimento.addEventListener("blur", (evento) => {
-  validadeDataNascimento(evento.target);
-});
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+}
 
-function validadeDataNascimento(input) {
-  const dataRecebida = new Date(input.value);
-  let mensagem = "";
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
 
-  if (!maiorQue18(dataRecebida)) {
-    mensagem = "Você deve ser maior de 18 anos para se cadastrar";
-  }
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensagem
+}
 
-  input.setCustomValidity(mensagem);
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+
+    input.setCustomValidity(mensagem)
 }
 
 function maiorQue18(data) {
-  const dataAtual = new Date();
-  const dataMais18 = new Date(
-    data.getUTCFullYear() + 18,
-    data.getUTCMonth(),
-    data.getUTCDate(),
-  );
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
-  return dataMais18 <= dataAtual;
+    return dataMais18 <= dataAtual
 }
